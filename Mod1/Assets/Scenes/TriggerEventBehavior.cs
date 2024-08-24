b6f08c1b74a7c8c3440605939d96496e78964fb5
Reese Nelson <reese_nelson1@gmail.com>
2024-08-23 22:31:14 -0600

    Adding some text float data script, need to add the modals in

---
+++ Mod1/Assets/Scenes/TriggerEventBehavior.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class TriggerEventBehavior : MonoBehaviour
+{
+   public UnityEvent triggerEnterEvent;
+
+   private void OnTriggerEnter(Collider other) 
+   {triggerEnterEvent.Invoke();}    
+   
+}
