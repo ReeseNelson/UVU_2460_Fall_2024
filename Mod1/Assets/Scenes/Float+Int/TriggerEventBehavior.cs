c1744adf432aa936b631223d6190578187297203
Reese Nelson <reese_nelson1@gmail.com>
2024-09-01 13:04:54 -0600

    lab 2 unity events

--- Mod1/Assets/Scenes/Float+Int/TriggerEventBehavior.cs
+++ Mod1/Assets/Scenes/Float+Int/TriggerEventBehavior.cs
@@ -5,6 +5,8 @@ public class TriggerEventBehavior : MonoBehaviour
    public UnityEvent triggerEnterEvent;
 
    private void OnTriggerEnter(Collider other) 
-   {triggerEnterEvent.Invoke();}    
+   {
+       triggerEnterEvent.Invoke();
+   }    
    
 }
