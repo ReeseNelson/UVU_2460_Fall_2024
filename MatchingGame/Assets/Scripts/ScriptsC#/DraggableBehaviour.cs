7d2cead4733a899496e86045419a2b0d204ecd4d
Reese Nelson <reese_nelson1@gmail.com>
2024-09-14 19:11:01 -0600

    Coroutine Update

--- MatchingGame/Assets/Scripts/ScriptsC#/DraggableBehaviour.cs
+++ MatchingGame/Assets/Scripts/ScriptsC#/DraggableBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine.Events;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DraggableBehaviour : MonoBehaviour
 {
     private Camera cameraObj;
     public bool draggable;
