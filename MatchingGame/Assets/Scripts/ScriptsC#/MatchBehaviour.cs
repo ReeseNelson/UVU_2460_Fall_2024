7e439c70b1adbfc2186e2bc9d735f012093e2168
Reese Nelson <reese_nelson1@gmail.com>
2024-09-25 21:07:02 -0600

    final challenge matching game 3

--- MatchingGame/Assets/Scripts/ScriptsC#/MatchBehaviour.cs
+++ MatchingGame/Assets/Scripts/ScriptsC#/MatchBehaviour.cs
@@ -16,11 +16,14 @@ public class MatchBehaviour : MonoBehaviour
         var otherID = tempObj.idObj;
         if (otherID == idObj)
         {
+            Debug.Log("Match");
             matchEvent.Invoke();
+            
 
         }
         else
         {
+           Debug.Log("NO Match");
            noMatchEvent.Invoke();
            yield return new WaitForSeconds(0.5f);
            noMatchDelayedEvent.Invoke();
